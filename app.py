340424bdf017b13b2851090d2196b6b4ee64de5d
Arpan Neupane <arpanneupane19@gmail.com>
2020-10-31 02:06:20 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,116 @@
+# Written by Arpan Neupane on October 30, 2020
+# Copyright ©️ Arpan Neupane 2020.
+# Refer to the README.md for more information.
+
+from flask import Flask, url_for, render_template, flash, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField, StringField, PasswordField
+from wtforms.validators import InputRequired, Length, ValidationError, Email
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_mail import Mail, Message
+import os
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+import datetime
+import time
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+app.config['SECRET_KEY'] = 'jfale!@#gys^&*(@jafd00193n'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_TODO")
+app.config['MAIL_PASSWORD'] = os.environ.get("PASSWORD_TODO")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.String(130), nullable=False)
+    complete = db.Column(db.Boolean)
+    todo_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    todo = db.relationship('Todo', backref='writer', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email(message="Invalid Email"), Length(max=50)],render_kw={"placeholder": "Email Address"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Username"})
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Password"})
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please choose a different one.")
+    
+    def validate_email(self, email):
+        existing_user_email = User.query.filter_by(email=email.data).first()
+        if existing_user_email:
+            raise ValidationError("That email address belongs to different user. Please choose a different one.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Username"})
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
+
+
+@app.route('/', methods=['GET', "POST"])
+@app.route('/home', methods=['GET', "POST"])
+@app.route('/sign-up', methods=['GET', "POST"])
+def home():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data)
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+
+	return render_template('home.html', form=form)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', form=form)
+
+if __name__ == '__main__':
+	app.run(debug=True)
