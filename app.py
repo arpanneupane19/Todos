a95fa925c75681d25b1197f86b61d18d7f4ed242
Arpan Neupane <arpanneupane19@gmail.com>
2020-10-31 02:33:03 -0500

    added user logins

--- app.py
+++ app.py
@@ -113,7 +113,26 @@ def home():
 @app.route('/login', methods=['GET','POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if bcrypt.check_password_hash(user.password, form.password.data):
+				login_user(user)
+				return redirect('dashboard')
 	return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('login'))
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+	return "This is the user dashboard."
 
 if __name__ == '__main__':
 	app.run(debug=True)
